90db9c998eda9be6dbee3df829a4ead9cb608457
giovani.rocha <giovanirocha2011@gmail.com>
2021-01-15 10:53:20 -0300

    Inserindo um array no arquivo

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-    echo "foda-se";
-    echo "Fazendo mais uma modificação no arquivo";
-
-    for(int i=0; i<10;i++){
-        echo "essa mensagem está sendo mostrada pela ", i, "º vez "<br>;
-    }
-
+    $nomes = [
+        "giovani", "fernanda"
+    ];
 ?>
