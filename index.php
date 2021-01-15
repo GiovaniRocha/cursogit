dc2f079ccc8040c2c3fb6fdd9d1cb77ae3946727
giovani.rocha <giovanirocha2011@gmail.com>
2021-01-15 13:54:02 -0300

    Modificando o arquivo para usar a função diff

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
     $nomes = [
-        "giovani", "fernanda"
+        "giovani", "fernanda", "Marcos", "Carlos"
     ];
+
+    for($i=0;$i<10;$i++){
+        echo "Essa msg foi mostrada ". $i . "vez";
+    }
+
+    echo "Mais uma linha escrita";
 ?>
