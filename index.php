e9ca06d3872bace226535620ee16c7978c15ab96
giovani.rocha <giovanirocha2011@gmail.com>
2021-01-19 09:17:40 -0300

    Nova modificação em index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
     $nomes = [
-        "giovani", "fernanda", "Marcos", "Carlos"
+        "xxxx", "yyyyyy", "zzzzzz", "tttttttt"
     ];
 
     for($i=0;$i<10;$i++){
         echo "Essa msg foi mostrada ". $i . "vez";
     }
 
-    echo "Mais uma linha escrita";
+    echo "yyyyyyyyy";
 ?>
